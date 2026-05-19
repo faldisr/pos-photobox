cd1c0cfd28e4107428f4e4c7bf2fd874282884da
rickybuldan <63239881+rickybuldan@users.noreply.github.com>
2026-05-19 21:24:19 +0700

    static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Wajib untuk static export ke shared hosting
   images: {
+    unoptimized: true, // Wajib diaktifkan jika menggunakan static export
     remotePatterns: [
       {
         protocol: "http",
