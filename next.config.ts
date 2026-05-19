afc79c2c13638d79816ee0c0b3bd3eb7d7309f11
rickybuldan <63239881+rickybuldan@users.noreply.github.com>
2026-05-19 23:22:28 +0700

    Update next.config.ts for export output and images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       {
         protocol: "http",
@@ -8,6 +12,6 @@ const nextConfig = {
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
