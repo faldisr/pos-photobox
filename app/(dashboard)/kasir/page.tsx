a88ad8f4a346d2a22eadefb3570e3bf42d463bfa
rickybuldan <63239881+rickybuldan@users.noreply.github.com>
2026-06-03 20:26:04 +0700

    Fix branchInfo prop in CartPanel component

--- app/(dashboard)/kasir/page.tsx
+++ app/(dashboard)/kasir/page.tsx
@@ -136,7 +136,7 @@ export default function KasirPage() {
           <CartPanel
             cart={cart}
             activeShift={activeShift}
-            branchInfo={BranchInfo}
+            branchInfo={branchInfo}
             onUpdateQuantity={updateQuantity}
             onRemove={removeFromCart}
             onClearCart={clearCart}
